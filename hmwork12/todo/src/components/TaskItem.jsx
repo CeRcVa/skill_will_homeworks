96aabda71f2327c0fb464fc81dd8ca353f39a5cb
CeRcVa <shotikogamer16@gmail.com>
2025-04-18 13:04:10 +0400

    ჰმ13
    განახლებული ჰმ12

--- hmwork12/todo/src/components/TaskItem.jsx
+++ hmwork12/todo/src/components/TaskItem.jsx
@@ -1,19 +1,21 @@
+import React from "react";
+
 function TaskItem({ task, onComplete, onDelete, onReturn }) {
-    return (
-      <div className="task-item">
-        <span>{task.text}</span>
-        {onComplete && (
-          <button onClick={() => onComplete(task.id)}>დასრულება</button>
-        )}
-        {onDelete && (
-          <>
-            <button onClick={() => onReturn(task.id)}>დააბრუნე</button>
-            <button onClick={() => onDelete(task.id)}>წაშლა</button>
-          </>
-        )}
-      </div>
-    );
-  }
-  
-  export default TaskItem;
-  
+  console.log("Render:", task.text);
+  return (
+    <div className="task-item">
+      <span>{task.text}</span>
+      {onComplete && (
+        <button onClick={() => onComplete(task.id)}>დასრულება</button>
+      )}
+      {onDelete && (
+        <>
+          <button onClick={() => onReturn(task.id)}>დააბრუნე</button>
+          <button onClick={() => onDelete(task.id)}>წაშლა</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default React.memo(TaskItem);
